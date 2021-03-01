690d8d3fe693c937accd4e3135640b97d778a9bf
Elshawa1 <a.shawa@outlook.com>
2021-03-01 18:36:13 +0200

    Add files via upload

---
+++ 2.ApplyTemplate.ps1
@@ -0,0 +1,2 @@
+﻿Connect-PnPOnline -Url "https://<<tenant>>.sharepoint.com/sites/RamadanApp"
+Apply-PnPProvisioningTemplate -Path ".\listemp_V16.xml"
---
+++ 3.HideLists.ps1
@@ -0,0 +1,6 @@
+﻿$listName = "QnA"
+$listName2= "UserPoints"
+$site = "https://<<tenant>>.sharepoint.com/sites/RamadanApp/"
+
+Connect-PnPOnline -Url $site -UseWebLogin
+Set-PnPList -Identity $listName -Hidden $true
